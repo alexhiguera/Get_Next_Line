718342829fea4043aeeb87e3a3c65785d8e337d1
alexhiguera <ahiguera@student.42urduliz.com>
2023-11-27 16:10:52 +0100

    update (tests): changes in main and Makefile to test

--- test/main.c
+++ test/main.c
@@ -6,7 +6,7 @@
 /*   By: ahiguera <ahiguera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 13:18:48 by ahiguera          #+#    #+#             */
-/*   Updated: 2023/11/24 14:10:13 by ahiguera         ###   ########.fr       */
+/*   Updated: 2023/11/27 15:35:17 by ahiguera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,21 @@ int	main(int argc, char **argv)
 {
 	int		fd;
 	char	*line;
+	int		i;
 
+	if (argc < 2)
+		return (0);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (0);
+	i = 1;
 	fd = open("prueba.txt", O_RDONLY);
-	printf("%d\n", fd);
-	line = get_next_line(fd);
-	printf("Linea %d: %s", 1, line);
-	free(line);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("Linea %d: %s", i, line);
+		free(line);
+		i++;
+	}
 	close(fd);
 	return (0);
 }
